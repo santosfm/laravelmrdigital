94c0ab20653b4bf65345fd3c48d5c0aa24ea7c01
FilipeSantos <24841572+santosfm@users.noreply.github.com>
2022-04-01 10:36:26 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-//Route::get('/index.php', [UserController::class, 'showWelcomePage'])->name('showWelcomePage'); //ADDED IT FOR HEROKU
 Route::get('/admin/main', [UserController::class, 'showWelcomePage'])->name('showWelcomePage');
 //Route::get('/users/user/{id}', [UserController::class, 'showUserById']);
-Route::get('/admin/users/create', [UserController::class, 'createUser']); //WORKS
-Route::post('/admin/users/create', [UserController::class, 'saveUser'])->name('createuser'); //WORKS
-Route::get('/admin/users', [UserController::class, 'showTheUsers'])->name('showusers'); //WORKS
-Route::get('/admin/users/view/{id}', [UserController::class, 'viewUser'])->name('viewuser'); //WORKS
-Route::post('/admin/users/update/{id}', [UserController::class, 'updateUser'])->name('updateuser'); //WORKS
-Route::post('/admin/users/delete/{id}', [UserController::class, 'deleteUser'])->name('deleteuser'); //WORKS
+Route::get('/admin/users/create', [UserController::class, 'createUser']); 
+Route::post('/admin/users/create', [UserController::class, 'saveUser'])->name('createuser'); 
+Route::get('/admin/users', [UserController::class, 'showTheUsers'])->name('showusers'); 
+Route::get('/admin/users/view/{id}', [UserController::class, 'viewUser'])->name('viewuser'); 
+Route::post('/admin/users/update/{id}', [UserController::class, 'updateUser'])->name('updateuser'); 
+Route::post('/admin/users/delete/{id}', [UserController::class, 'deleteUser'])->name('deleteuser'); 
 
-route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest'); 
+route::get('/login', [AuthController::class, 'login'])->name('login'); //->middleware('guest'); 
 route::post('/login', [AuthController::class, 'signin'])->name('signin');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
